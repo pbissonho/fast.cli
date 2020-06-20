41235eff0d245814958bd4e366961f3a2b7eacae
pbissonho <pedro.bissonho@gmail.com>
2020-06-20 18:11:01 -0300

    adding tests

--- test/install_package_test.dart
+++ test/install_package_test.dart
@@ -1,16 +1,24 @@
 import 'package:fast/actions/add_pachage.dart';
+import 'package:fast/core/exceptions.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('shoud install package', () async {
-    // var addPackageAction = AddPackage('koin', '/test/resources/_pubspec.yaml', false);
-    // await addPackageAction.execute();
-  });
-
-  test('shoud not install package', () async {
-    var addPackageAction = AddPackage('konidfsdf', 'pubspec.yaml', false);
+    var addPackageAction =
+        AddPackage('koin', 'test/resources/_pubspec.yaml', false);
     await addPackageAction.execute();
   });
 
-  test('install command', () async {});
+   test('shoud install a dev package', () async {
+    var addPackageAction =
+        AddPackage('koin', 'test/resources/_pubspec.yaml', true);
+    await addPackageAction.execute();
+  });
+
+  test('shoud not install package', () async {
+    var addPackageAction =
+        AddPackage('konidfsdf', 'test/resources/_pubspec.yaml', false);
+    expect(() => addPackageAction.execute(),
+        throwsA((value) => value is FastException));
+  });
 }
